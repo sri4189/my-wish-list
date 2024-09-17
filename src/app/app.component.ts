0993ea227c2d562452795a2c837d66086da42ec0
sri4189 <99990596+sri4189@users.noreply.github.com>
2024-09-17 20:29:24 +0530

    Angular NG model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItems';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my wish list';
+  items: WishItem[] =  [
+    new WishItem("Learn Angular"),
+    new WishItem("Get Coffee", true),
+    new WishItem("Find grass which cuts itself")
+  ]
+  newWishText: any = '';
+  listFilters: string ='0';
+  visibleItems: WishItem[] = this.items;
+
+  toggleItem(item : WishItem)
+  {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
+  addNewWish()
+  {
+  this.items.push( new WishItem(this.newWishText));
+  this.newWishText = '';
+  }
+  filterChange(value:any)
+  {
+    debugger;
+   if(value ==0)
+   {
+    this.visibleItems = this.items;
+   }
+   else if(value ==1){
+    this.visibleItems = this.items.filter(item => !item.isComplete);
+   }
+   else {
+    this.visibleItems = this.items.filter(item => item.isComplete);
+   }
+  }
 }
